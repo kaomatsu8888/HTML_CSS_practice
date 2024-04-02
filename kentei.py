e7b1aae152525811e233a6b54c58a4ae8d83d0ed
LAPTOP-28DR15R7\frontier-Python <m.kaoru0830@gmail.com>
2024-04-02 11:37:31 +0900

    kentei

--- kentei.py
+++ kentei.py
@@ -1,7 +1,10 @@
-def fugafuga(title,content = 'default_content', number = 4):
-    content = 'content'
-    print(title, end=' ')
-    print(content, end=' ')
-    print(number)
- 
-fugafuga(title = 'title_default', content = 'None', number = 5) 
+def function(number, default_arg_list=[]):
+    default_arg_list.append(number)
+    return default_arg_list
+
+
+print(function(1))  # [1]
+print(function(2,[3,4]))  # [3, 4, 2]
+print(function(3))  # [1, 3]
+print(function(4,[5,6]))  # [5, 6, 4]
+print(function(5))  # [1, 3, 5]
