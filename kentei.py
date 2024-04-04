00c5dfad399730c730b67d33686053da19934e70
LAPTOP-28DR15R7\frontier-Python <m.kaoru0830@gmail.com>
2024-04-04 11:32:48 +0900

    html

--- kentei.py
+++ kentei.py
@@ -1,25 +1,11 @@
-class Duck:
-    def __init__(self):
-        self.birdsong = 'ガーガー'
-
-    def sing(self):
-        birdsong = 'カーカー'
-        print(birdsong)
-        print(self.birdsong)
-        self.birdsong = 'ピヨピヨ'
-        print(self.birdsong)
-        print(birdsong)
+import unittest # unittestとは、Pythonの標準ライブラリで、テストを自動化するためのモジュールです。
 
 
-duck = Duck()
-duck.sing()
+class TestApp(unittest.TestCase):
+    def test_one(self):
+        actual = "Sunday"
+        expected = "Monday"
+        self.assertEqual(actual, expected) #結果を比較するので==ではなく、assertEqualを使う.assertの意味は「主張する」という意味です。
 
-# 実行結果の順番解説
-# 1. birdsong = 'カーカー'
-# 2. print(birdsong) -> カーカー
-# 3. print(self.birdsong) -> ガーガー
-# 4. self.birdsong = 'ピヨピヨ'
-# 5. print(self.birdsong) -> ピヨピヨ
-# 6. print(birdsong) -> カーカー
-# 7. print(self.birdsong) -> ピヨピヨ
+# python -m unittestコマンドでテストを実行する.-mの意命はmoduleを指定するという意味です。
 
